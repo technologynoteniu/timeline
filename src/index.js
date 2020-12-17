d6dc1a38bec18c7ad17cf6515c092736709a2fea
John-Lin <linton.tw@gmail.com>
2020-12-18 02:05:32 +0800

    change title with tags

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     for (var i = 0; i < posts.length; i++) {
         console.log(posts[i]);
         if (i % 2 !== 0) {
-            $(".timeline").append('<li><div class="direction-r"><div class="flag-wrapper"><span class="hexa"></span><span class="flag">' + 'Lorem Occaecat' + '</span><span class="time-wrapper"><span class="time">' + posts[i].year + '</span></span></div><div class="desc"><div class="fb-post" data-href="' + posts[i].link + '" data-show-text="true" data-width="400"></div></div></div></li>');
+            $(".timeline").append('<li><div class="direction-r"><div class="flag-wrapper"><span class="hexa"></span><span class="flag">' + posts[i].tag + '</span><span class="time-wrapper"><span class="time">' + posts[i].year + '</span></span></div><div class="desc"><div class="fb-post" data-href="' + posts[i].link + '" data-show-text="true" data-width="400"></div></div></div></li>');
         } else {
-            $(".timeline").append('<li><div class="direction-l"><div class="flag-wrapper"><span class="hexa"></span><span class="flag">' + 'Lorem Occaecat' + '</span><span class="time-wrapper"><span class="time">' + posts[i].year + '</span></span></div><div class="desc"><div class="fb-post" data-href="' + posts[i].link + '" data-show-text="true" data-width="400"></div></div></div></li>');
+            $(".timeline").append('<li><div class="direction-l"><div class="flag-wrapper"><span class="hexa"></span><span class="flag">' + posts[i].tag + '</span><span class="time-wrapper"><span class="time">' + posts[i].year + '</span></span></div><div class="desc"><div class="fb-post" data-href="' + posts[i].link + '" data-show-text="true" data-width="400"></div></div></div></li>');
         }
     }
 });
--- src/posts.js
+++ src/posts.js
@@ -1,91 +1,91 @@
 var posts = [
   {
     "year": "2020/09/05",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps Demo 及介紹",
     "link": "https://www.facebook.com/technologynoteniu/posts/113756857125122"
   },
   {
     "year": "2020/09/11",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps 帶來的痛點與反思",
     "link": "https://www.facebook.com/technologynoteniu/posts/118905326610275"
   },
   {
     "year": "2020/09/11",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "單體式架構 v.s 微服務架構",
     "link": "https://www.facebook.com/technologynoteniu/posts/119240203243454"
   },
   {
     "year": "2020/09/12",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "Kubernetes CI/CD 工具雷達報告",
     "link": "https://www.facebook.com/technologynoteniu/posts/119347956566012"
   },
   {
     "year": "2020/09/22",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "CI/CD 部署策略比較",
     "link": "https://www.facebook.com/technologynoteniu/posts/133250015175806"
   },
   {
     "year": "2020/10/16",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps 到底解決了什麼問題",
     "link": "https://www.facebook.com/technologynoteniu/posts/144275300739944"
   },
   {
     "year": "2020/10/17",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "什麼是 GitOps?",
     "link": "https://www.facebook.com/technologynoteniu/posts/144729410694533"
   },
   {
     "year": "2020/10/17",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "淺談 GitOps 過往",
     "link": "https://www.facebook.com/technologynoteniu/posts/145132500654224"
   },
   {
     "year": "2020/10/18",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "淺談 GitOps 過往",
     "link": "https://www.facebook.com/technologynoteniu/posts/145132500654224"
   },
   {
     "year": "2020/10/19",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps 工具的選擇",
     "link": "https://www.facebook.com/technologynoteniu/posts/145512047282936"
   },
   {
     "year": "2020/10/20",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps 實作上的挑戰",
     "link": "https://www.facebook.com/technologynoteniu/posts/145888253911982"
   },
   {
     "year": "2020/11/26",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "GitOps 中如何做好 Secret Management",
     "link": "https://www.facebook.com/technologynoteniu/posts/168101821690625"
   },
   {
     "year": "2020/11/29",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "使用 DORA 幫你的 DevOps 效率進行測量",
     "link": "https://www.facebook.com/technologynoteniu/posts/170022318165242"
   },
   {
     "year": "2020/11/30",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "使用 CircleCI & ArgoCD 來幫 k8s 打造 GitOps",
     "link": "https://www.facebook.com/technologynoteniu/posts/170963384737802"
   },
   {
     "year": "2020/12/05",
-    "tags": "cicd",
+    "tag": "CI/CD",
     "title": "使用 Flagger 幫你的應用程式導入金絲雀部署",
     "link": "https://www.facebook.com/technologynoteniu/posts/176167600884047"
   }

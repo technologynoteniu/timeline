c1880a258cf5f40796449fefd9fbf4954634c758
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-12-18 17:09:41 +0800

    Add the meta for csv and skip the first line in sh

--- convert.sh
+++ convert.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 DIR_PREFIX="awesome-notes/docs"
 rm -rf $DIR_PREFIX/kubernetes $DIR_PREFIX/iac
 mkdir -p $DIR_PREFIX/kubernetes
 mkdir -p $DIR_PREFIX/iac
 
 IFS=$'\n'
-for line in $(cat posts.csv | tr -d '\r'); do
+for line in $(cat posts.csv | tail -n +2 | tr -d '\r'); do
     year=$(echo $line | cut -d, -f 1)
     tag=$(echo $line | cut -d, -f 2)
     title=$(echo $line | cut -d, -f 3)
